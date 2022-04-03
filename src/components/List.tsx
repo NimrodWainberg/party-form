a65c3c78fd7d7013debf58b1cb546ae5b4d16059
Nimrod <n.wainberg@gmail.com>
2022-04-03 16:36:21 +0300

    List of people invited to the party

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -9,12 +9,27 @@ import React from "react";
     }[]
   }
 
-
-const List = (props: IProps) => {
-    return (
-        <div>
-        Hello
+// FC- functional component
+// <IProps>- props of type IProps
+const List: React.FC<IProps> = ({people}) => {
+// : JSX.Element[]- return an array of JSX Element
+  const renderList = (): JSX.Element[] => {
+    return people.map(person => {
+      return ( <li className="List">
+        <div className="List-header">
+          <img className="List-img" src={person.url}></img>
+          <h2>{person.name}</h2>
         </div>
+        <p>{person.age} years old</p>
+        <p  className="List-note"> {person.note}</p>
+      </li>
+      )
+    })
+  }
+    return (
+        <ul>
+          {renderList()}
+        </ul>
     )
 }
 
